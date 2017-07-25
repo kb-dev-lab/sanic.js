42df0862571039ec5f5704e5b2db5a1ca6cfa64b
aamulumi <florian@kauder.fr>
2017-07-26 00:20:24 +0200

    fix(exp): Add missing Array.find() require

--- bench/array/index.js
+++ bench/array/index.js
@@ -3,6 +3,7 @@
 module.exports = {
     every: require('./every'),
     filter: require('./filter'),
+    find: process.env.EXPERIMENTAL ? require('./find') : undefined,
     forEach: require('./forEach'),
     indexOf: process.env.EXPERIMENTAL ? require('./indexOf') : undefined,
     lastIndexOf: process.env.EXPERIMENTAL ? require('./lastIndexOf') : undefined,
